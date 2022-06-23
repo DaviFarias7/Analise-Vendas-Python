37715278961dc3660b5d18652c05e038d34ddbaf
DaviFarias7 <86566715+DaviFarias7@users.noreply.github.com>
2022-06-22 22:40:47 -0300

    Update SMSVendas.py

--- SMSVendas.py
+++ SMSVendas.py
@@ -20,7 +20,7 @@ for mes in lista_meses:
         vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 45000,'Vendas'].values[0]
         print(f'No mês {mes} alguém bateu a meta. Vendedor:{vendedor}, Vendas:{vendas}')
         message = client.messages.create(
-            to="+5583996384873",
+            to="+558399999999",
             from_="+13862591123",
             body=f'No mês {mes} alguém bateu a meta. Vendedor:{vendedor}, Vendas:{vendas}')
 
